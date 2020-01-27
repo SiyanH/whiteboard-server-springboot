ea1e5ed094084569ed4b995f08e072e75dcb5429
hsy <he.siya@husky.neu.edu>
2020-01-27 12:39:15 -0500

    Create user admin service client

---
+++ src/main/webapp/services/user.service.client.js
@@ -0,0 +1,41 @@
+function AdminUserServiceClient() {
+    this.createUser = createUser;
+    this.findAllUsers = findAllUsers;
+    this.findUserById = findUserById;
+    this.deleteUser = deleteUser;
+    this.updateUser = updateUser;
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/hsy6337/users';
+    let self = this;
+
+    function createUser(user) {
+        return fetch(self.url, {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                "content-type": "application/json"
+            }
+        }).then(response => response.json())
+    }
+
+    function findAllUsers() {
+        return fetch(self.url).then(response => response.json())
+    }
+
+    function findUserById(id) {
+        return fetch(`${self.url}/${id}`).then(response => response.json())
+    }
+
+    function updateUser(id, user) {
+        return fetch(`${self.url}/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(user),
+            headers: {
+                "content-type": "application/json"
+            }
+        }).then(response => response.json())
+    }
+
+    function deleteUser(id) {
+        return fetch(`${self.url}/${id}`, {method: "DELETE"})
+    }
+}
