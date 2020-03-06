7d1f2198c02fde1f87589cb58997bda60193a47f
hsy <he.siya@husky.neu.edu>
2020-03-06 00:01:10 -0500

    Implement topic and widget controllers

---
+++ src/main/java/com/example/cs5610spring2020springbootsiyanhe/controllers/TopicController.java
@@ -0,0 +1,55 @@
+package com.example.cs5610spring2020springbootsiyanhe.controllers;
+
+import com.example.cs5610spring2020springbootsiyanhe.models.Topic;
+import com.example.cs5610spring2020springbootsiyanhe.models.Widget;
+import com.example.cs5610spring2020springbootsiyanhe.services.TopicService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class TopicController {
+  @Autowired
+  TopicService service;
+
+  @PostMapping("/api/lessons/{lid}/topics")
+  public Topic createTopic(@PathVariable("lid") String lid, @RequestBody Topic topic) {
+    return service.createTopic(lid, topic);
+  }
+
+  @GetMapping("/api/lessons/{lid}/topics")
+  public List<Topic> findTopicsForLesson(@PathVariable("lid") String lid) {
+    return service.findTopicsForLesson(lid);
+  }
+
+  @PutMapping("/api/topics/{tid}")
+  public int updateTopic(@PathVariable("tid") int tid, @RequestBody Topic topic) {
+    return service.updateTopic(tid, topic);
+  }
+
+  @DeleteMapping("/api/topics/{tid}")
+  public int deleteTopic(@PathVariable("tid") int tid) {
+    return service.deleteTopic(tid);
+  }
+
+
+  @GetMapping("/api/topics")
+  public List<Topic> findAllTopics() {
+    return service.findAllTopics();
+  }
+
+  @GetMapping("/api/topics/{tid}")
+  public Topic findTopicById(@PathVariable("tid") int tid) {
+    return service.findTopicById(tid);
+  }
+}
--- src/main/java/com/example/cs5610spring2020springbootsiyanhe/controllers/WidgetController.java
+++ src/main/java/com/example/cs5610spring2020springbootsiyanhe/controllers/WidgetController.java
@@ -3,6 +3,7 @@ package com.example.cs5610spring2020springbootsiyanhe.controllers;
 import com.example.cs5610spring2020springbootsiyanhe.models.Widget;
 import com.example.cs5610spring2020springbootsiyanhe.services.WidgetService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,25 +18,26 @@ import java.util.List;
 @RestController
 @CrossOrigin(origins = "*")
 public class WidgetController {
-  private WidgetService service = WidgetService.getInstance();
+  @Autowired
+  WidgetService service;
 
   @PostMapping("/api/topics/{tid}/widgets")
-  public Widget createWidget(@PathVariable("tid") String tid, @RequestBody Widget widget) {
+  public Widget createWidget(@PathVariable("tid") int tid, @RequestBody Widget widget) {
     return service.createWidget(tid, widget);
   }
 
   @GetMapping("/api/topics/{tid}/widgets")
-  public List<Widget> findWidgetsForTopic(@PathVariable("tid") String tid) {
+  public List<Widget> findWidgetsForTopic(@PathVariable("tid") int tid) {
     return service.findWidgetsForTopic(tid);
   }
 
   @PutMapping("/api/widgets/{wid}")
-  public int updateWidget(@PathVariable("wid") String wid, @RequestBody Widget widget) {
+  public int updateWidget(@PathVariable("wid") int wid, @RequestBody Widget widget) {
     return service.updateWidget(wid, widget);
   }
 
   @DeleteMapping("/api/widgets/{wid}")
-  public int deleteWidget(@PathVariable("wid") String wid) {
+  public int deleteWidget(@PathVariable("wid") int wid) {
     return service.deleteWidget(wid);
   }
 
@@ -45,7 +47,7 @@ public class WidgetController {
   }
 
   @GetMapping("/api/widgets/{wid}")
-  public Widget findWidgetById(@PathVariable("wid") String wid) {
+  public Widget findWidgetById(@PathVariable("wid") int wid) {
     return service.findWidgetById(wid);
   }
 
